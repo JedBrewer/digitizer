98a8c36507bbc34e91ab19ec59cfe77b28c59d8a
Jed <jed.brewer@gmail.com>
2018-06-15 21:22:40 -0700

    Add Circle to RootWidget Refactor Configuration handler  -Create custom subclass GUIConfig

--- python/digitizergui.py
+++ python/digitizergui.py
@@ -1,15 +1,22 @@
 #:kivy 1.10.0
+from math import sqrt
+from os.path import basename
+from re import sub
 
-from configparser import ConfigParser
 from kivy.app import App
-from kivy.uix.widget import Widget
+from kivy.config import Config
 from kivy.core.window import Window
+from kivy.graphics import Color, Line
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
-from kivy.config import Config
+from kivy.uix.widget import Widget
 
 # disable multitouch
+from guiconfig import GUIConfig
+
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
+
+config_filename = sub(r'.py', r'.ini', basename(__file__))
 
 
 class Point(Widget):
@@ -39,26 +46,92 @@ class Grid(GridLayout):
                 self.add_widget(point)
 
 
+class Circle(Widget):
+    rad = 0
+    mx = 0
+    my = 0
+
+    circle = None
+
+    def __init__(self, thickness, is_bounded, color):
+        super().__init__()
+
+        self._thickness = thickness
+        self._is_bounded = is_bounded
+        self._color = color
+
+    def redraw(self):
+        with self.canvas:
+            Color(*self._color)
+
+            if not self._is_bounded or \
+                    (self.x < self.mx - self.rad and self.right > self.mx + self.rad) and \
+                    (self.y < self.my - self.rad and self.top > self.my + self.rad):
+                if self.circle:
+                    self.canvas.remove(self.circle)
+
+                self.circle = Line(circle=(self.mx, self.my, self.rad), width=self._thickness)
+
+
 class RootWidget(FloatLayout):
     def __init__(self):
         super().__init__()
 
-        # read configuration settings
-        config = ConfigParser()
-        config.read("digitizergui.ini")
+        self.default_radius_ratio = 0.2
+        self.default_radius = 0
 
-        # set grid dimensions
-        dims = config['Dimensional Values']
-        cols = int(dims['h_res'])
-        rows = int(dims['v_res'])
-        point_coverage = float(dims['point_coverage'])
+        config = GUIConfig()
+        config.read(config_filename)
+
+        # retrieve Grid parameters
+        grid_params = config['Grid']
+        cols = grid_params.h_res
+        rows = grid_params.v_res
+        point_coverage = grid_params.point_coverage
+
+        # retrieve Circle parameters
+        circle_params = config['Circle']
+        thickness = circle_params.line_thick
+        is_bounded = circle_params.is_bounded
+        color = circle_params.color
 
         # add point grid
         self.grid = Grid(point_coverage=point_coverage, cols=cols, rows=rows)
+        self.circle = Circle(thickness=thickness, is_bounded=is_bounded, color=color)
 
         self.add_widget(self.grid)
+        self.add_widget(self.circle)
 
         del config
+
+    def on_touch_down(self, touch):
+        # default_radius = min(self.grid.width, self.grid.height) * self.default_radius_ratio
+        if touch.button == 'left':
+            default_radius = min(self.grid.width / self.grid.cols,
+                                 self.grid.height / self.grid.rows) * self.default_radius_ratio
+            self.circle.mx, self.circle.my = touch.x, touch.y
+            self.circle.rad = default_radius
+            self.circle.redraw()
+            self.circle.opacity = 1
+
+    def on_touch_move(self, touch):
+        if touch.button == 'left':
+            cx, cy = self.circle.mx, self.circle.my
+            self.circle.rad = sqrt((touch.x - cx) ** 2 + (touch.y - cy) ** 2)
+            self.circle.redraw()
+
+    def on_touch_up(self, touch):
+        self.analyze()
+
+    def analyze(self):
+        self.highlight_points()
+        self.request_analysis()
+
+    def highlight_points(self):
+        pass
+
+    def request_analysis(self):
+        pass
 
 
 class DigitizerGUI(App):
---
+++ python/guiconfig.py
@@ -0,0 +1,70 @@
+from configparser import ConfigParser
+from re import findall
+
+
+class GUIConfig(ConfigParser):
+    # Grid parameters
+    cols = 0
+    rows = 0
+    point_coverage = 0.0
+
+    # Circle parameters
+    thickness = 1
+    is_bounded = True
+    color = (0, 0, 1)
+
+    def read(self, filename):
+        super().read(filename)
+
+        self.config_filename = filename
+
+        grid_params = self.get_section('Grid')
+
+        grid_params.h_res = self.int('Grid', 'h_res')
+        grid_params.v_res = self.int('Grid', 'v_res')
+        grid_params.point_coverage = self.float('Grid', 'point_coverage')
+
+        circle_params = self.get_section('Circle')
+
+        circle_params.line_thick = self.int('Circle', 'line_thick')
+        circle_params.is_bounded = self.bool('Circle', 'is_bounded')
+        circle_params.color = self.tuple('Circle', 'color', 3)
+
+    def get_section(self, label):
+        try:
+            return self[label]
+        except KeyError:
+            raise KeyError('Section "' + label + '" not found in configuration file "' + self.config_filename + '"')
+
+    def get_val(self, section, label):
+        try:
+            return self[section][label]
+        except KeyError:
+            raise KeyError('Value "' + label + '" not found in section "' + section + '" configuration file "' + self.config_filename + '"')
+
+    def int(self, section, label):
+        val = self.get_val(section, label)
+        try:
+            return int(val)
+        except ValueError:
+            raise ValueError('Value "' + label + '" in section "' + section + '" must be an integer. Instead, got: "' + val + '"')
+
+    def float(self, section, label):
+        val = self.get_val(section, label)
+        try:
+            return float(val)
+        except ValueError:
+            raise ValueError('Value "' + label + '" in section "' + section + '" must be a float. Instead, got: "' + val + '"')
+
+    def bool(self, section, label):
+        val = self.get_val(section, label)
+        if val == "True" or val == "False":
+            return val == "True"
+        raise ValueError('Value "' + label + '" in section "' + section + '" must be True or False. Instead, got: "' + val + '"')
+
+    def tuple(self, section, label, size):
+        val = self.get_val(section, label)
+        valtup = tuple(map(float, findall('[0-9.]+', val)))
+        if len(valtup) == size:
+            return valtup
+        raise ValueError('Value "' + label + '" in section "' + section + '" must be a ' + str(size) + '-tuple. Instead, got: "' + val + '"')
